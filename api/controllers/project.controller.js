ae7ff322c921269347278e6822efdc8197108717
mist4kes <sihoang36@gmail.com>
2022-11-09 15:59:42 +0700

    Update project.controller.js

--- api/controllers/project.controller.js
+++ api/controllers/project.controller.js
@@ -32,8 +32,8 @@ async function findByText(req, res) {
 
 async function findAll(req, res) {
   const pageOptions = {
-    page: parseInt(req.query.page, 10) || 0,
-    limit: parseInt(req.query.limit, 10) || 1000,
+    page: parseInt(req.query.page, 10) || 1,
+    limit: parseInt(req.query.limit, 10) || 6,
     lastPage: 1 + parseInt(await Project.countDocuments()),
   };
 
